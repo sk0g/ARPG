baed3c5f2cb946e3a62ec85140bb3bdfe93bfe30
sk0g <ani.mehta@mail.com>
2021-08-08 15:15:41 +1000

    Tidy

--- Assets/Scripts/Player/Weapons/BaseWeapon.cs
+++ Assets/Scripts/Player/Weapons/BaseWeapon.cs
@@ -1,28 +1,37 @@
+#region
+
 using Interfaces;
 using UnityEngine;
+
+#endregion
 
 namespace Player.Weapons
 {
     [RequireComponent(typeof(Collider))]
     public abstract class BaseWeapon : MonoBehaviour, IAttack
     {
-        [SerializeField] private AnimatorOverrideController weaponAnimations;
-        public AnimatorOverrideController WeaponAnimations   { 
+        [SerializeField] AnimatorOverrideController weaponAnimations;
+
+        public AnimatorOverrideController WeaponAnimations
+        {
             get => weaponAnimations;
             private set => weaponAnimations = value;
         }
-        
+
         // TODO: actually have this access the collider automatically
-        [SerializeField] private Collider weaponCollider;
-        public Collider WeaponCollider   { 
+        [SerializeField] Collider weaponCollider;
+
+        public Collider WeaponCollider
+        {
             get => weaponCollider;
             private set => weaponCollider = value;
         }
-        
+
         protected string Name => gameObject.name;
 
         // every weapon needs a name 
-        [SerializeField] private string description;
+        [SerializeField] string description;
+
         public string Description
         {
             get => description;
@@ -30,8 +39,10 @@ namespace Player.Weapons
         }
 
         // how far a weapon can reach, excludes arm length and leg? length
-        [SerializeField] private float range;
-        protected float Range   { 
+        [SerializeField] float range;
+
+        protected float Range
+        {
             get => range;
             private set => range = value;
         }
