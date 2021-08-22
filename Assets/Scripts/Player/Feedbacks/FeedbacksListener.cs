769c09911d66e5fa08abfc05f2652b84bd55498f
sk0g <ani.mehta@mail.com>
2021-08-22 21:35:04 +1000

    Update feedbacks to give player an on hit feedback

--- Assets/Scripts/Player/Feedbacks/FeedbacksListener.cs
+++ Assets/Scripts/Player/Feedbacks/FeedbacksListener.cs
@@ -3,11 +3,26 @@ using UnityEngine;
 
 namespace Player.Feedbacks
 {
+[RequireComponent(typeof(MMFeedbacks))]
 public class FeedbacksListener : MonoBehaviour
 {
+    MMFeedbacks _feedbacks;
+
+    void Awake()
+    {
+        _feedbacks = GetComponent<MMFeedbacks>();
+        _feedbacks.Initialization();
+
+        print($"{name} has feedback count {_feedbacks.Feedbacks.Count}");
+    }
+
     void PlayFeedbacks(string feedbackName)
     {
-        if (feedbackName == name) { GetComponent<MMFeedbacks>()?.PlayFeedbacks(); }
+        if (feedbackName == name && _feedbacks)
+        {
+            _feedbacks.PlayFeedbacks();
+            print($"playing feedback in {name} with arg {feedbackName}");
+        }
     }
 }
 }
