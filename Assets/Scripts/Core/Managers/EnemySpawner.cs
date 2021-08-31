19a5e6a072a86adf72969282c6ab6b99215cec4b
sk0g <ani.mehta@mail.com>
2021-08-31 22:55:28 +1000

    Fix enemies spawning inside the player at times

--- Assets/Scripts/Core/Managers/EnemySpawner.cs
+++ Assets/Scripts/Core/Managers/EnemySpawner.cs
@@ -15,11 +15,17 @@ public class EnemySpawner : MonoBehaviour
     [Header("General")] [SerializeField] [Range(1f, 20f)]
     float unitSpawnRadius = 20f;
 
+    [SerializeField] [Range(2f, 20f)] float minimumSpawnDistance = 10f;
+
+    GameObject _player;
+
     void Awake()
     {
         if (swordsman == null) { print("Swordsman prefab is null!"); }
 
         if (plane == null) { print("Plane is null!"); }
+
+        _player = GameObject.FindWithTag("Player");
     }
 
     void OnEnable()
@@ -45,14 +51,29 @@ public class EnemySpawner : MonoBehaviour
         {
             var sm = Instantiate(swordsman, gameObject.transform);
 
-            var currentPosition = sm.transform.position;
-            currentPosition.x += RandomFloat();
-            currentPosition.z += RandomFloat();
-
-            sm.transform.SetPositionAndRotation(currentPosition, sm.transform.rotation);
+            sm.transform.SetPositionAndRotation(GetValidSpawnPoint(), sm.transform.rotation);
         }
     }
 
+    Vector3 GetValidSpawnPoint()
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            var newTransform = new Vector3(RandomFloat(), gameObject.transform.position.y, RandomFloat());
+            if (SpawnPointValid(newTransform)) { return newTransform; }
+        }
+
+        print("Giving up and returning anyway");
+        return new Vector3(RandomFloat(), gameObject.transform.position.y, RandomFloat());
+    }
+
     float RandomFloat() => (Random.value * 2 * unitSpawnRadius) - unitSpawnRadius;
+
+    bool SpawnPointValid(Vector3 point) => DistanceToPlayerOk(point) && PointIsAboveStage(point);
+
+    bool PointIsAboveStage(Vector3 point) => Physics.Raycast(point, Vector3.down, 2f);
+
+    bool DistanceToPlayerOk(Vector3 point) =>
+        Vector3.Distance(_player.transform.position, point) >= minimumSpawnDistance;
 }
 }
