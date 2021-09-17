3dd433086cc3e8e45efb0959b331add57af08a26
sk0g <ani.mehta@mail.com>
2021-09-18 01:12:43 +1000

    Tidy up direction modifier

--- Assets/Scripts/Core/DirectionModifier.cs
+++ Assets/Scripts/Core/DirectionModifier.cs
@@ -1,26 +1,24 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+namespace Core
+{
 public class DirectionModifier : MonoBehaviour
 {
     [Range(5f, 25f)] [SerializeField] float rayCheckDistance = 5.0f;
     [Range(0.1f, 5f)] [SerializeField] float rayThiccness = 1.0f;
     [Range(45, 360f)] [SerializeField] float totalLookAngle = 360f;
-    [Range(1, 6)] [SerializeField] int rayCount = 1;
+    [Range(1, 6)] [SerializeField] int raySpacingAngle = 16;
+
+    int _totalRayCount;
 
     Ray forwardRay;
-
-    int rayCountStepSize = 16;
-
-    public int RayCount
-    {
-        get => rayCount * rayCountStepSize;
-        set => rayCount = value;
-    }
 
     void Awake()
     {
         forwardRay = new Ray(transform.position, transform.forward);
+        _totalRayCount = 360 / raySpacingAngle;
     }
 
     void FixedUpdate()
@@ -40,36 +38,33 @@ public class DirectionModifier : MonoBehaviour
 
     float FindBestLookAngle()
     {
-        List<Ray> ray = CreateSideRays();
+        var r = RaysToTest().FirstOrDefault(CanMoveForward);
 
-        int i = 0;
-        foreach (Ray r in ray)
-        {
-            i++;
-            if (CanMoveForward(r))
-            {
-                float angle = Vector3.Angle(Vector3.forward, r.direction);
-                // Debug.DrawRay(r.origin, r.direction*3.0f);
-                transform.rotation *= Quaternion.Euler(0, angle, 0);
-            }
-        }
+        float angle = Vector3.Angle(Vector3.forward, r.direction);
+        // Debug.DrawRay(r.origin, r.direction*3.0f);
 
+        transform.Rotate(new Vector3(0, 1, 0), angle);
         return 0;
     }
 
-    List<Ray> CreateSideRays()
+    IEnumerable<Ray> RaysToTest()
     {
-        List<Ray> castRays = new();
+        var rayOrigin = transform.position;
+        var forward = transform.forward;
 
-        for (int i = 0; i < RayCount; i++)
+        for (var i = 0; i < _totalRayCount; i++)
         {
-            Ray r = new Ray();
-            r.origin = transform.position;
-            r.direction = Quaternion.Euler(0, (i * totalLookAngle / RayCount) - (totalLookAngle / 2), 0)
-                          * transform.forward;
-            castRays.Add(r);
-        }
+            var angleMultiplier = (int) Mathf.Floor((i + 1) / 2);
 
-        return castRays;
+            if (i % 2 == 0) // even - left rotate
+            {
+                yield return new Ray(rayOrigin, Quaternion.Euler(0, -raySpacingAngle * angleMultiplier, 0) * forward);
+            }
+            else // odd - right rotate
+            {
+                yield return new Ray(rayOrigin, Quaternion.Euler(0, raySpacingAngle * angleMultiplier, 0) * forward);
+            }
+        }
     }
+}
 }
